372f6284b5ff4cb745cc838471c6ac16a0570f4c
willilsl <32499493+willilsl@users.noreply.github.com>
2019-02-25 15:33:54 +0100

    test jdk

--- src/no/hiof/softwareEngineering/Main.java
+++ src/no/hiof/softwareEngineering/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Halggggglo igjen");
+
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
 }
