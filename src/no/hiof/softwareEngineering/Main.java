aa6fe0fc3aef803a4a9c6c142478d1b2c16ceda6
Enis Jasharaj <enis_jasharaj@hotmail.com>
2019-02-25 15:39:37 +0100

    Test

--- src/no/hiof/softwareEngineering/Main.java
+++ src/no/hiof/softwareEngineering/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Halggggglo igjen");
-
         for (int i = 0; i < 10; i++){
             System.out.println(i);
         }
