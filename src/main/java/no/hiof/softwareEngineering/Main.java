a29f3546226b4ef6949b67065cd4b4face0f5ba3
Enis Jasharaj <enis_jasharaj@hotmail.com>
2019-02-27 17:42:06 +0100

    Maven og Junit funker som det skal, husk å skru på auto import!

--- src/main/java/no/hiof/softwareEngineering/Main.java
+++ src/main/java/no/hiof/softwareEngineering/Main.java
@@ -5,8 +5,5 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Halggggglo igjen");
-        for (int i = 0; i < 10; i++){
-            System.out.println(i);
-        }
     }
 }
