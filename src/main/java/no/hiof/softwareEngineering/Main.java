775f137a8daac80e22d801b7fcc210d5a24fd11f
Enis Jasharaj <enis_jasharaj@hotmail.com>
2019-03-03 14:01:25 +0100

    La til 11-jdk på Maven-prosjektet.

--- src/main/java/no/hiof/softwareEngineering/Main.java
+++ src/main/java/no/hiof/softwareEngineering/Main.java
@@ -1,9 +1,13 @@
 package no.hiof.softwareEngineering;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Halggggglo igjen");
+        int[] nums = IntStream.range(1, 101).toArray();
+        Arrays.stream(nums).forEach(System.out::println);
     }
 }
