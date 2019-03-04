e39f2763948f749c28d8121fd9e9828ea31b548e
willilsl <32499493+willilsl@users.noreply.github.com>
2019-03-04 13:46:34 +0100

    Update Main.java

--- src/main/java/no/hiof/softwareEngineering/Main.java
+++ src/main/java/no/hiof/softwareEngineering/Main.java
@@ -1,14 +1,9 @@
 package no.hiof.softwareEngineering;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int[] nums = IntStream.range(1, 101).toArray();
-        Arrays.stream(nums).forEach(System.out::println);
+
         System.out.println("test");
     }
 }
