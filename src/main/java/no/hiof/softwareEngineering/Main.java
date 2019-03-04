3829b64622494e2ce3ce0950c90fcc831e2122a9
Ingrid <ingrid.e.dahl@hiof.no>
2019-03-04 13:59:02 +0100

    test push opprett konto

--- src/main/java/no/hiof/softwareEngineering/Main.java
+++ src/main/java/no/hiof/softwareEngineering/Main.java
@@ -1,13 +1,8 @@
 package no.hiof.softwareEngineering;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int[] nums = IntStream.range(1, 101).toArray();
-        Arrays.stream(nums).forEach(System.out::println);
+        System.out.println("Helo");
     }
 }
