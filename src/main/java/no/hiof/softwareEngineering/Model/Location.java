3b8cb54264c21992c333a4c691399b343f463c01
Enis Jasharaj <enis_jasharaj@hotmail.com>
2019-03-22 11:30:40 +0100

    Auto stash before revert of "Merge remote-tracking branch 'origin/opprettekonto'"

---
+++ src/main/java/no/hiof/softwareEngineering/Model/Location.java
@@ -0,0 +1,27 @@
+package no.hiof.softwareEngineering.Model;
+
+public class Location {
+    private String city;
+    private String street;
+
+    public Location(String city, String street) {
+        this.city = city;
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+}
---
+++ src/main/java/no/hiof/softwareEngineering/Model/Ticket.java
@@ -0,0 +1,49 @@
+package no.hiof.softwareEngineering.Model;
+
+
+public class Ticket {
+    private String ticketCode;
+    private double price;
+    private Customer customer;
+
+    public Ticket(String ticketCode, double price) {
+        this.ticketCode = ticketCode;
+        this.price = price;
+    }
+
+    public Ticket(String ticketCode, double price, Customer customer){
+        this.ticketCode = ticketCode;
+        this.price = price;
+        this.customer = customer;
+    }
+
+
+    public String getTicketCode() {
+        return ticketCode;
+    }
+
+    public void setTicketCode(String ticketCode) {
+        this.ticketCode = ticketCode;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "    Bilettkode: " + ticketCode + "\n    Pris: " + price + "\n   Billett eier: " + customer.getPerson().firstname + " " + customer.getPerson().lastname + "\n";
+    }
+}
