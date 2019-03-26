4d2248f9f446a871b856b7421ba8e023c678d65d
Enis Jasharaj <enis_jasharaj@hotmail.com>
2019-03-26 11:49:19 +0100

    Fjerna vekk scanner funksjonalitet fra login over til MainView.

--- src/main/java/no/hiof/softwareEngineering/Model/Login.java
+++ src/main/java/no/hiof/softwareEngineering/Model/Login.java
@@ -8,31 +8,7 @@ import java.util.Scanner;
 
 public abstract class Login {
 
-    public static <T extends Person> T userLogin(ArrayList<T> userList){
-        Scanner scanner = new Scanner(System.in);
-        T user;
-
-        for(int i = 0; i < 3; i++){
-            System.out.print("Epost: ");
-            String inputEmail = scanner.nextLine();
-
-            System.out.print("Passord: ");
-            String inputPassword = scanner.nextLine();
-
-            user = loginValidation(inputEmail, inputPassword, userList);
-            if(user != null){
-               // System.out.println("\nDu er nå logget inn.");
-                return user;
-            } else
-                System.out.println("Feil epost og/eller passord, vennligst prøv igjen.");
-        }
-
-        System.out.println("Alle forsøk er brukt opp, vennligst prøv igjen senere.");
-        scanner.close();
-        return null;
-    }
-
-    private static <T extends Person> T loginValidation(String email, String password, ArrayList<T> userList){
+    private static Organizer loginValidation(String email, String password){
         return userList.stream().filter(org -> email.equals(org.getEmail()) && password.equals(org.getPassword())).findFirst().orElse(null);
     }
 
--- src/main/java/no/hiof/softwareEngineering/View/LoginView.java
+++ src/main/java/no/hiof/softwareEngineering/View/LoginView.java
@@ -1,4 +1,34 @@
 package no.hiof.softwareEngineering.View;
 
+import no.hiof.softwareEngineering.Model.Organizer;
+import no.hiof.softwareEngineering.Model.Person;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class LoginView {
+
+    public static Organizer userLogin(){
+        Scanner scanner = new Scanner(System.in);
+        Organizer user;
+
+        for(int i = 0; i < 3; i++){
+            System.out.print("Epost: ");
+            String inputEmail = scanner.nextLine();
+
+            System.out.print("Passord: ");
+            String inputPassword = scanner.nextLine();
+
+            user = loginValidation(inputEmail, inputPassword, userList);
+            if(user != null){
+                // System.out.println("\nDu er nå logget inn.");
+                return user;
+            } else
+                System.out.println("Feil epost og/eller passord, vennligst prøv igjen.");
+        }
+
+        System.out.println("Alle forsøk er brukt opp, vennligst prøv igjen senere.");
+        scanner.close();
+        return null;
+    }
 }
