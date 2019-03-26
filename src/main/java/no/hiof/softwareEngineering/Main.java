5d2016ad421ab280ef1ce4a5f1dab6ebb0e3cc14
willilsl <32499493+willilsl@users.noreply.github.com>
2019-03-26 13:52:27 +0100

    obj fix

--- src/main/java/no/hiof/softwareEngineering/Main.java
+++ src/main/java/no/hiof/softwareEngineering/Main.java
@@ -2,12 +2,23 @@ package no.hiof.softwareEngineering;
 
 import no.hiof.softwareEngineering.Controller.Login;
 import no.hiof.softwareEngineering.Model.*;
+import no.hiof.softwareEngineering.View.MainView;
 
 import java.time.LocalDate;
+
+import static no.hiof.softwareEngineering.Lists.ListManager.eventList;
+import static no.hiof.softwareEngineering.Lists.ListManager.organizerList;
 
 public class Main {
 
     public static void main(String[] args) {
+        Event TestEvent = new Event("Sopptur med Grete", "Friluft", "Ta med riktige klær og godt humør", 0, 20, LocalDate.of(2019, 03,29), new Location("Halden", "B R A veien 6d"), 1, "admin");
+        Customer testCustomer = new Customer("bruker", "bruker", "bruker", "bruker");
+        Organizer TestOrganizer = new Organizer("admin", "admin", "admin", "admin");
+        organizerList.add(TestOrganizer);
+        eventList.add(TestEvent);
 
+        MainView run = new MainView();
+        run.userOptions();
     }
 }
