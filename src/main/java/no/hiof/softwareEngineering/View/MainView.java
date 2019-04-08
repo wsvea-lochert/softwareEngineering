5335615b484da7714f17c42647b63d85cee39d94
willilsl <32499493+willilsl@users.noreply.github.com>
2019-04-08 13:43:14 +0200

    refactor of register method
    refactor of register account methods into one method

--- src/main/java/no/hiof/softwareEngineering/View/MainView.java
+++ src/main/java/no/hiof/softwareEngineering/View/MainView.java
@@ -1,10 +1,6 @@
 package no.hiof.softwareEngineering.View;
-import no.hiof.softwareEngineering.Controller.CustomerDriver;
 import no.hiof.softwareEngineering.Controller.OrganizerDriver;
-import no.hiof.softwareEngineering.Model.Customer;
-
 import java.util.Scanner;
-
 import static no.hiof.softwareEngineering.View.CustomerView.runCustomerPart;
 import static no.hiof.softwareEngineering.View.OrganizerView.runOrganizer;
 
@@ -27,10 +23,10 @@ public class MainView {
                     runCustomerPart();
                     break;
                 case 3:
-                    registerCustomerAccont();
+                    registerAccount(option);
                     break;
                 case 4:
-                    registerOrganizerAccont();
+                    registerAccount(option);
                     break;
                 case 5:
                     break;
@@ -45,15 +41,8 @@ public class MainView {
     // ToDo: legge inn diverse sjekker på input. Se hva som ligger på Person
     // ToDo: Sjekk på om mailadressen ligger inne allerede?
 
-    private void registerOrganizerAccont(){
+    private void registerAccount(int option){
         Scanner input = new Scanner(System.in);
-
-        System.out.print("Firmanavn: ");
-        String companyName = input.nextLine();
-
-        //Register new organizer account
-        System.out.print("Organisasjonsnummer: ");
-        String orgNumber = input.nextLine();
 
         System.out.print("Epostadresse: ");
         String email = input.nextLine();
@@ -61,28 +50,25 @@ public class MainView {
         System.out.print("Passord: ");
         String password = input.nextLine();
 
-        OrganizerDriver.createNewOrganizerAccount(orgNumber, companyName, email, password);
+        if(option == 4){
+            System.out.print("Firmanavn: ");
+            String companyName = input.nextLine();
+
+            //Register new organizer account
+            System.out.print("Organisasjonsnummer: ");
+            String orgNumber = input.nextLine();
+
+            OrganizerDriver.createNewOrganizerAccount(orgNumber, companyName, email, password);
+        }
+        else if (option == 3){
+            System.out.print("Fornavn ");
+            String firstname = input.nextLine();
+
+            System.out.print("Etternavn: ");
+            String lastname = input.nextLine();
+
+
+            OrganizerDriver.createNewOrganizerAccount(firstname,lastname, email, password);
+        }
     }
-
-    // ToDo: legge inn diverse sjekker på input. Se hva som ligger på Person
-    // ToDo: Sjekk på om mailadressen ligger inne allerede?
-
-    private void registerCustomerAccont(){
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Fornavn ");
-        String firstname = input.nextLine();
-
-        System.out.print("Etternavn: ");
-        String lastname = input.nextLine();
-
-        System.out.print("Epostadresse: ");
-        String email = input.nextLine();
-
-        System.out.print("Passord: ");
-        String password = input.nextLine();
-
-        OrganizerDriver.createNewOrganizerAccount(firstname,lastname, email, password);
-    }
-
 }
