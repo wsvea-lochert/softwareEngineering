11dcb1d870e8d8a62a007793a32b840164b645b1
willilsl <32499493+willilsl@users.noreply.github.com>
2019-04-08 13:49:16 +0200

    Update ListManager.java
    add comments

--- src/main/java/no/hiof/softwareEngineering/Lists/ListManager.java
+++ src/main/java/no/hiof/softwareEngineering/Lists/ListManager.java
@@ -7,10 +7,11 @@ import no.hiof.softwareEngineering.Model.Organizer;
 import java.util.ArrayList;
 
 public class ListManager {
-    public static ArrayList<Event> eventList = new ArrayList<>();
-    public static ArrayList<Ticket> soldTickets = new ArrayList<>();
-    public static ArrayList<Organizer> organizerList = new ArrayList<>();
-    public static ArrayList<Customer> customerList = new ArrayList<>();
-    public static ArrayList<Ticket> availableTickets = new ArrayList<>();
+
+    public static ArrayList<Event> eventList = new ArrayList<>(); // a list that holds all events in the system
+    public static ArrayList<Ticket> soldTickets = new ArrayList<>(); // a list that holds all sold tickets
+    public static ArrayList<Organizer> organizerList = new ArrayList<>(); // a list that holds all organizers in the system
+    public static ArrayList<Customer> customerList = new ArrayList<>(); // a list that holds all customers in the system
+    public static ArrayList<Ticket> availableTickets = new ArrayList<>(); // a list that holds all unsold tickets that can be bought
 
 }
