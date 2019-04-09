6bdf4b7554f20753cba7d01f0a57625783c93b35
willilsl <dmuwilliamsl@gmail.com>
2019-04-09 23:28:25 +0200

    Betalings stub fullført
    :+1:

--- src/main/java/no/hiof/softwareEngineering/Controller/Payment.java
+++ src/main/java/no/hiof/softwareEngineering/Controller/Payment.java
@@ -1,29 +1,55 @@
 package no.hiof.softwareEngineering.Controller;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Payment {
 
-    public static boolean paymentDriver(String cardNumber, int cvc, LocalDate cardDate){
+    public static String fixDate(String date){
+        String replaceString = date.replace("/", "-");
+        return "01-" + replaceString;
+    }
+
+    public static boolean checkCardNumber(String cardnumber){
+        String regex = "^(?:[0-9]{16})$";
+        Pattern patter = Pattern.compile(regex);
+        Matcher matcher = patter.matcher(cardnumber);
+
+        return cardnumber.length() == 16 && matcher.matches();
+    }
+
+    public static boolean checkDatePattern(String date){
+        String regex = "^[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$"; // MM/YY
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(date);
+
+        return matcher.matches();
+    }
+
+    public static boolean checkCVC(int cvc){
+        return cvc > 99 && cvc < 1000;
+    }
+
+    public static boolean checkDate(String Date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yy");
         LocalDate today = LocalDate.now();
-        if (cardNumber.length() == 16 && cvc > 99 && cvc <= 999){
-            if(cardDate.getYear() > today.getYear()){
-                System.out.println("succsses");
+        LocalDate cardDate = LocalDate.parse(Date, formatter);
+
+
+        if(cardDate.getYear() > today.getYear()){
+            return true;
+        }
+        else if(cardDate.getYear() < today.getYear()){
+            return false;
+        }
+        else if (cardDate.getYear() == today.getYear()){
+            if (cardDate.getMonthValue() >= today.getMonthValue()){
                 return true;
             }
-            else if(cardDate.getYear() < today.getYear()){
-                System.out.println("Error");
+            else{
                 return false;
-            }
-            else if (cardDate.getYear() == today.getYear()){
-                if (cardDate.getMonthValue() >= today.getMonthValue()){
-                    System.out.println("succsses");
-                    return true;
-                }
-                else{
-                    System.out.println("error");
-                    return false;
-                }
             }
         }
         return false;
--- src/main/java/no/hiof/softwareEngineering/View/PaymentView.java
+++ src/main/java/no/hiof/softwareEngineering/View/PaymentView.java
@@ -1,29 +1,66 @@
 package no.hiof.softwareEngineering.View;
 
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static no.hiof.softwareEngineering.Controller.Payment.*;
+
 public class PaymentView {
+    static Scanner input = new Scanner(System.in);
 
-    public static void runPayment(){
-        Scanner input = new Scanner(System.in);
-        System.out.print("Cardnumber: ");
-        String cardNumebr = input.nextLine();
+    public static boolean runPayment(){
+       // String cardnumber = resciveCardNumber();
+        //String cardDate = resciveDate();
+      //  resciveCVC();
 
-        String regex = "^(?:[0-9]{2})?[0-9]{2}/[0-3]?[0-9]$";
-        Pattern pattern = Pattern.compile(regex);
-
-        System.out.print("date: ");
-        String date = input.nextLine();
-        Matcher matcher = pattern.matcher(date);
-
-        while(!matcher.matches()){
-            System.out.print("Ugyldig input: ");
-            date = input.nextLine();
-            matcher = pattern.matcher(date);
-        }
+        return resciveCardNumber() && resciveDate() && resciveCVC();
     }
+
+    public static boolean resciveCVC(){
+        System.out.print("CVC: ");
+        int cvc = input.nextInt();
+
+        while(!checkCVC(cvc)){
+            System.out.print("CVC er for kort eller for lang, prøv igjen: ");
+            cvc = input.nextInt();
+        }
+        return true;
+        //return cvc;
+    }
+
+    public static boolean resciveDate(){
+        System.out.print("date: ");
+        String tempDate = input.nextLine();
+
+        while(!checkDatePattern(tempDate)){
+            tempDate = input.nextLine();
+        }
+
+        String date = fixDate(tempDate);
+
+        if (!checkDate(date)) {
+            System.out.println("Ugyldig dato, prøv igjen.");
+            resciveDate();
+        }
+        return true;
+    }
+
+    public static boolean resciveCardNumber(){
+        System.out.print("Skirv inn kort nummer(16 siffer): ");
+        String cardNumber = input.nextLine();
+
+        while(!checkCardNumber(cardNumber)){
+            System.out.print("Feil, Prøv igjen: ");
+            cardNumber = input.nextLine();
+        }
+        return true;
+    }
+
+
 
 
 }
