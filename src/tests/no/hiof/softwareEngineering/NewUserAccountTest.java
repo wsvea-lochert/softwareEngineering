14c772cf8b6ad00796c968cbf28119b377a8e28a
Ingrid <ingrid.e.dahl@hiof.no>
2019-04-10 14:39:42 +0200

    Laget tester for NewUserAccount

--- src/tests/no/hiof/softwareEngineering/NewUserAccountTest.java
+++ src/tests/no/hiof/softwareEngineering/NewUserAccountTest.java
@@ -6,32 +6,52 @@ import no.hiof.softwareEngineering.Model.Organizer;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 
 public class NewUserAccountTest {
-    private Customer testCustomer;
-    private Customer emptyTestCustomer;
-    private Customer wrongTestCustomer;
-    private Organizer testOrganizer;
-    private Organizer emptyTestOrganizer;
-    private Organizer wrongTestOrganizer;
 
-    @BeforeEach
-    public void createTestObjects()  {
-        testCustomer = new Customer("Test", "Testesen", "test@epost.no", "abcABC123");
-        emptyTestCustomer = new Customer("", "", "", "");
-        wrongTestCustomer = new Customer("Tehfjkuyytrehbgnjmuhgfdcvbnhyujkmnhgfdertghyujkijhu", "Tehfjkuyytrehbgnjmuhgfdcvbnhyujkmnhgfdertghyujkijhu", "test", "abc");
-        testOrganizer = new Organizer("Test AS", "test@test.no", "ABCabc123", "Testhild", "Testerius");
-        emptyTestOrganizer = new Organizer("", "", "", "", "");
-        wrongTestOrganizer = new Organizer("Test AS", "test", "abc", "Tehfjkuyytrehbgnjmuhgfdcvbnhyujkmnhgfdertghyujkijhu", "Tehfjkuyytrehbgnjmuhgfdcvbnhyujkmnhgfdertghyujkijhu");
+    @ParameterizedTest
+    @ValueSource(strings = {"Test", "T", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"})
+    public void checkStringLengthTestReturnsTrue(String checkString) {
+        boolean result = NewUserAccount.checkStringLength(1, 50, checkString);
+        Assert.assertTrue(result);
     }
 
-    @Test
-    public void checkStringLengthTest() {
-        boolean resultTrue = NewUserAccount.checkStringLength(1, 50, testCustomer.getFirstname());
-        Assert.assertTrue(resultTrue);
+    @ParameterizedTest
+    @ValueSource(strings = {"", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"})
+    public void checkStringLengthTestReturnsFalse(String checkString) {
+        boolean result = NewUserAccount.checkStringLength(1, 50, checkString);
+        Assert.assertFalse(result);
+    }
 
-        boolean resultFalse = NewUserAccount.checkStringLength(1, 50, emptyTestCustomer.getFirstname());
-        Assert.assertFalse(resultFalse);
+    @ParameterizedTest
+    @ValueSource(strings = {"test@epost.abcd", "a@b.cd"})
+    public void checkEmailTestReturnsTrue(String checkString) {
+        boolean result = NewUserAccount.checkEmail(checkString);
+        Assert.assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"test", "test@epost", "", "test.no", "tøst@æøå.øø", "test@epost.n", "test@epost.abcde", "@.no"})
+    public void checkEmailTestReturnsFalse(String checkString) {
+        boolean result = NewUserAccount.checkEmail(checkString);
+        Assert.assertFalse(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abcABC1!", "!ABCDabc", "1aB2cD3eF", "123%&/dD"})
+    public void checkPasswordTestReturnsTrue(String checkString) {
+        boolean result = NewUserAccount.checkPassword(checkString);
+        Assert.assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abABC1!", "aaaaaaaaaa", "aaaaAAAAAA", "1111!!!!!!!", "aaaaaaa1111111", "AAAAAAA!!!!!!!"})
+    public void checkPasswordTestReturnsFalse(String checkString) {
+        boolean result = NewUserAccount.checkPassword(checkString);
+        Assert.assertFalse(result);
     }
 
 }
